e343b4cae586eded30a4dd2104b81614b8d33ff1
huyf <724826175@qq.com>
2022-11-01 11:08:38 +0800

    新增移动端2

---
+++ health_mobile/src/main/java/com/tony/health_mobile/controller/SetMealController.java
@@ -0,0 +1,37 @@
+package com.tony.health_mobile.controller;
+
+import com.tony.health_common.constant.MessageConstant;
+import com.tony.health_common.entity.Result;
+import com.tony.health_common.pojo.Setmeal;
+import com.tony.health_interface.service.SetMealService;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 套餐管理
+ */
+@RestController
+@RequestMapping("/setmeal")
+public class SetMealController {
+    @DubboReference
+    private SetMealService setMealService;
+
+    /**
+     * 获取所有套餐
+     */
+    @PostMapping("/getAllSetmeal")
+    public Result getAllSetmeal() {
+        try {
+            List<Setmeal> list = setMealService.getAllSetmeal();
+            return new Result(true, MessageConstant.GET_SETMEAL_LIST_SUCCESS, list);
+        } catch (Exception e) {
+            //调用失败
+            e.printStackTrace();
+            return new Result(false, MessageConstant.GET_SETMEAL_LIST_FAIL);
+        }
+    }
+}
